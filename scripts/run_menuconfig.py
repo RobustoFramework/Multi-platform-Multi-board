2a95c21f0f2bc3161abf75fe868f902998711350
Nicklas Börjesson <nicklasb@gmail.com>
2023-04-07 23:02:15 +0200

    Always overwrite config

--- scripts/run_menuconfig.py
+++ scripts/run_menuconfig.py
@@ -4,5 +4,5 @@ import sys
 print(os.getcwd())
 # First, get our variables from the environment
 pio_env = sys.argv[1]
-os.system("cp -n scripts/Kconfig.template ./Kconfig.{0}".format(pio_env))
+os.system("cp -f scripts/Kconfig.template ./Kconfig.{0}".format(pio_env))
 os.system("KCONFIG_CONFIG=./.config.{0} menuconfig Kconfig.{0}".format(pio_env))
